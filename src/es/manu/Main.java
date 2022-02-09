7080d563cecef3582068ecbd203a5461cd191965
Manuel José Tornero <manueljosetornero@hotmail.com>
2022-02-09 09:48:35 +0100

    calculadora terminá

--- src/es/manu/Main.java
+++ src/es/manu/Main.java
@@ -20,21 +20,39 @@ public class Main {
             switch (opcion) {
                 case 1:
                     System.out.println("Vamo a sumar");
-                    int sumando1, sumando2;
-                    System.out.print("Sumando 1: ");
-                    sumando1 = entrada.nextInt();
-                    System.out.print("Sumando 2: ");
-                    sumando2 = entrada.nextInt();
-                    System.out.print("Suma: " + (sumando1 + sumando2));
+                    int sumando1, sumandos, total = 0;
+
+                    System.out.print("¿Cuántos números quieres sumar? ");
+                    sumandos = entrada.nextInt();
+
+                    for (int i = 1; i <= sumandos; i++){
+                        System.out.print("Sumando " + i + ": ");
+                        sumando1 = entrada.nextInt();
+                        total += sumando1;
+                    }
+                    System.out.println("Suma: " + total);
                     break;
                 case 2:
                     System.out.println("Vamo a restar");
+                    int minuendo, sustraendo;
+
+                    System.out.print("Restando 1: ");
+                    minuendo = entrada.nextInt();
+
+                    System.out.print("Restando 2: ");
+                    sustraendo = entrada.nextInt();
+
+                    System.out.println("Resta: " + (minuendo - sustraendo));
                     break;
                 case 0:
                     System.out.println("Ya te va?? Despídete bien no??");
                     break;
             }
+            entrada.nextLine();
             System.out.println();
+
+            System.out.print("Pulsa intro para continuar...");
+            entrada.nextLine();
         } while (opcion != 0);
     }
 }
